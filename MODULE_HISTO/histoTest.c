9526c3ed5b928dd775bd2bd29b3c4d2025b0a8b1
rouillearthur <rouillearthur@protonmail.com>
2018-12-23 21:39:19 +0100

    suppression de insert_cell car fonctionne pas

--- MODULE_HISTO/histoTest.c
+++ MODULE_HISTO/histoTest.c
@@ -3,6 +3,18 @@
 #include "histogram.h"
 
 int main() {
-  printf("Hello!");
+	cell list=NULL;
+	cell tmp=NULL;
+
+	list=create_cell(20,NULL);
+  list=insert_cell(list,2);
+
+	tmp=list;
+	while (tmp) {
+		printf("%d %d -> ",list->B, list->freq);	
+		tmp=tmp->next;
+	}
+	printf("\n");
+
   return 0;
 }
--- MODULE_HISTO/histogram.c
+++ MODULE_HISTO/histogram.c
@@ -1,39 +1,30 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "histogram.h"
 
 cell create_cell(int B, cell next) {
   cell new_cell=malloc(sizeof(new_cell));
-  new_cell->freq=1;
-  new_cell->B=B;
-  new_cell->next=next;
+	new_cell->freq=1;
+	new_cell->B=B;
+	new_cell->next=next;
   return new_cell;
 }
 
 cell insert_cell(cell head, int B) {
-  int exists=0;
-  cell cell;
 
-  /* On vérifie si B est présent dans la liste head */
-  while(head->next!=NULL && exists==0) {
-    if(head->B==B)
-      exists=1;
-    else
-      head=head->next;
-  }
 
-  /* Si B existe, alors on incrémente sa valeur de 1 */
-  if (exists==1) {
-    head->B=B+1;
-    cell=head;
-  }
-  /* Sinon on ajoute la cellule contenant B de freq 1*/
-  else {
-    cell=create_cell(1,head);
-  }
   return cell;
 }
 
 cell delete_list(cell list) {
+  /* head est la tête "courante" de la liste */
+  cell head;
 
+  /* on supprime dynamiquement la tête de liste */
+  while (list) {
+  	head=list;
+  	list=list->next;
+  	free(head);
+  }
   return list;
 }
