8a957a0cb461dfe3eef3df2a871fbcd57fd0db59
rouillearthur <rouillearthur@protonmail.com>
2018-12-26 18:25:10 +0100

    insert_list presque au point !

--- MODULE_HISTO/histogram.c
+++ MODULE_HISTO/histogram.c
@@ -11,38 +11,38 @@ cell create_cell(int B, cell next) {
 }
 
 cell insert_cell(cell head, int B) {
-  int exists;
+	int exists;
 
-  cell prec_cell;
-  cell new_cell;
-  cell next_cell;
+	cell prec_cell;
+	cell new_cell;
+	cell next_cell;
 
-  exists=0;
-  prec_cell=head;
-  new_cell=NULL;
-  next_cell=head->next;
+	exists=0;
+	prec_cell=head;
+	new_cell=NULL;
+	next_cell=head->next;
 
   /* On vérifie si B est présent dans la liste head */
- 	while(next_cell!=NULL && (next_cell->B)<=B && exists==0) {
-    if(next_cell->B==B) {
-    	/* Si B existe, alors on incrémente sa valeur de 1 et 
-    	on sort de la boucle */
-      exists=1;
-    	(prec_cell->B)++;
-    	printf("A %d\n",prec_cell->B);
+	while(next_cell!=NULL && (next_cell->B)<=B && exists==0) {
+		if(next_cell->B==B) {
+			/* Si B existe, alors on incrémente sa valeur de 1 et 
+			on sort de la boucle */
+			exists=1;
+			(prec_cell->B)++;
+			printf("A %d\n",prec_cell->B);
     }
-    else {
-    	prec_cell=next_cell;
-    	next_cell=next_cell->next;
-    	printf("B\n");
+		else {
+				prec_cell=next_cell;
+				next_cell=next_cell->next;
+				printf("B\n");
     }
   }
 
   /* Si B n'existe pas, on ajoute la cellule contenant B de freq 1 
   entre prec_cell et next_cell */
   if (exists==0) {
-    prec_cell->next=new_cell;
-    new_cell=create_cell(B,next_cell);
+		prec_cell->next=new_cell;
+		new_cell=create_cell(B,next_cell);
   }
   return head;
 }
